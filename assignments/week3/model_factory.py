14e54a0f1a01f4ab5aa3958060bfdc2731809d09
“nikolasm2000” <m.nikolas2000@gmail.com>
2023-02-07 17:01:37 -0500

    updates

--- assignments/week3/model_factory.py
+++ assignments/week3/model_factory.py
@@ -15,8 +15,8 @@ def create_model(input_dim: int, output_dim: int) -> MLP:
         MLP: The created model.
 
     """
-    hidden_size = 32
-    hidden_count = 1
+    hidden_size = 128
+    hidden_count = 3
 
     return MLP(
         input_dim,
