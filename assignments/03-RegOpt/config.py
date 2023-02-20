1ffb3cd5f6a38b3d2443482b6bd808849ac797e7
“nikolasm2000” <m.nikolas2000@gmail.com>
2023-02-20 18:26:18 -0500

    changes:

--- assignments/03-RegOpt/config.py
+++ assignments/03-RegOpt/config.py
@@ -6,21 +6,38 @@ from torchvision.transforms import Compose, Normalize, ToTensor
 
 
 class CONFIG:
+    # batch_size = 64
+    # num_epochs = 10
+    # initial_learning_rate = 0.01
+    # initial_weight_decay = 0
 
     batch_size = 64
-    num_epochs = 2
-    initial_learning_rate = 0.001
+    num_epochs = 10
+    initial_learning_rate = 0.01
     initial_weight_decay = 0
+
+    step_size = 10
+    gamma = 0.1
+
+    # ?? 128 batch_size -> higher test acc (just right, more is worse)??
 
     lrs_kwargs = {
         # You can pass arguments to the learning rate scheduler
         # constructor here.
+        "gamma": gamma,
+        "num_epochs": num_epochs,
+        "initial_learning_rate": initial_learning_rate,
+        "step_size": step_size,
+        "batch_size": batch_size,
+        # 'initial_weight_decay': initial_weight_decay
     }
 
     optimizer_factory: Callable[
         [nn.Module], torch.optim.Optimizer
     ] = lambda model: torch.optim.Adam(
-        model.parameters(), lr=CONFIG.initial_learning_rate, weight_decay=CONFIG.initial_weight_decay,
+        model.parameters(),
+        lr=CONFIG.initial_learning_rate,
+        weight_decay=CONFIG.initial_weight_decay,
     )
 
     transforms = Compose(
--- assignments/03-RegOpt/scheduler.py
+++ assignments/03-RegOpt/scheduler.py
@@ -4,7 +4,21 @@ from torch.optim.lr_scheduler import _LRScheduler
 
 
 class CustomLRScheduler(_LRScheduler):
-    def __init__(self, optimizer, last_epoch=-1):
+    """
+    A custom learning rate scheduler that extends the PyTorch _LRScheduler class.
+    """
+
+    def __init__(
+        self,
+        optimizer,
+        step_size,
+        num_epochs,
+        initial_learning_rate,
+        initial_weight_decay,
+        batch_size,
+        last_epoch=-1,
+        gamma=0.1,
+    ):
         """
         Create a new scheduler.
 
@@ -13,12 +27,46 @@ class CustomLRScheduler(_LRScheduler):
 
         """
         # ... Your Code Here ...
+        self.step_size = step_size
+        self.gamma = gamma
+        self.batch_size = batch_size
+        self.num_epochs = num_epochs
+        self.initial_learning_rate = initial_learning_rate
+        self.initial_weight_decay = initial_weight_decay
+
         super(CustomLRScheduler, self).__init__(optimizer, last_epoch)
+        # leave alone
 
     def get_lr(self) -> List[float]:
+        """
+        Get the current learning rate for each parameter group.
+
+        Returns:
+            A list of floats representing the current learning rate for each
+            parameter group in the optimizer.
+        """
+
         # Note to students: You CANNOT change the arguments or return type of
         # this function (because it is called internally by Torch)
 
         # ... Your Code Here ...
         # Here's our dumb baseline implementation:
-        return [i for i in self.base_lrs]
+        # return [i for i in self.base_lrs]
+
+        learning_rates = []
+        lr = self.initial_learning_rate
+
+        # DID NOT HELP
+        # # lr = lr / (self.batch_size ** 0.5)
+
+        for group in self.optimizer.param_groups:
+            # DID NOT HELP
+            # if (self.last_epoch == 0) or (self.last_epoch % self.step_size != 0):
+            #     lr = lr
+            # lr = (self.last_epoch // self.step_size) * lr
+
+            lr = self.gamma * lr
+            learning_rates.append(lr)
+
+        # Return the learning rate
+        return learning_rates
