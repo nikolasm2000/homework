059152aa456a40421235e7515b2bec5636cc274f
“nikolasm2000” <m.nikolas2000@gmail.com>
2023-03-15 14:14:01 -0400

    initial try

--- assignments/04-Convs/model.py
+++ assignments/04-Convs/model.py
@@ -1,9 +1,42 @@
 import torch
+import torch.nn as nn
+import torch.nn.functional as F
 
 
 class Model(torch.nn.Module):
-    def __init__(self, num_channels: int, num_classes: int) -> None:
-        ...
+    """
+    A CNN model for image classification.
+    """
+
+    def __init__(
+        self,
+        num_channels: int,
+        num_classes: int,
+    ) -> None:
+        super().__init__()
+        self.conv1 = nn.Conv2d(num_channels, 6, 5)
+        self.pool = nn.MaxPool2d(2, 2)
+        self.conv2 = nn.Conv2d(6, 16, 5)
+        self.fc1 = nn.Linear(16 * 5 * 5, 120)
+        self.fc2 = nn.Linear(120, 84)
+        self.fc3 = nn.Linear(84, num_classes)
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
-        ...
+        """
+        Forward pass through the dataset.
+
+        Arguments:
+            X (np.ndarray): The input data.
+
+        Returns:
+            np.ndarray: The predicted output.
+
+        """
+
+        x = self.pool(F.relu(self.conv1(x)))
+        x = self.pool(F.relu(self.conv2(x)))
+        x = torch.flatten(x, 1)
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)
+        return x
