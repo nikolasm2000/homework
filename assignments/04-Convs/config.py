c3bd4681b6490ed7aa3a6d43ee4ac559fc0864ae
“nikolasm2000” <m.nikolas2000@gmail.com>
2023-03-15 20:32:31 -0400

    retry

--- assignments/04-Convs/config.py
+++ assignments/04-Convs/config.py
@@ -6,8 +6,8 @@ from torchvision.transforms import Compose, ToTensor
 
 
 class CONFIG:
-    batch_size = 64
-    num_epochs = 1
+    batch_size = 16
+    num_epochs = 3
 
     optimizer_factory: Callable[
         [nn.Module], torch.optim.Optimizer
