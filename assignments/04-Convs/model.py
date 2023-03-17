a87f838053984b880c72100572604d86d82c467d
“nikolasm2000” <m.nikolas2000@gmail.com>
2023-03-17 20:52:41 +0000

    edit

--- assignments/04-Convs/model.py
+++ assignments/04-Convs/model.py
@@ -14,10 +14,10 @@ class Model(torch.nn.Module):
         num_classes: int,
     ) -> None:
         super().__init__()
-        self.conv1 = nn.Conv2d(num_channels, 6, 5)
+        self.conv1 = nn.Conv2d(num_channels, 12, 5)
         self.pool = nn.MaxPool2d(2, 2)
         self.dropout = nn.Dropout(0.1)
-        self.fc1 = nn.Linear(1176, 120)
+        self.fc1 = nn.Linear(2352, 120)
         self.fc3 = nn.Linear(120, num_classes)
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
